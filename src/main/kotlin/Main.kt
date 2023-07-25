e520bf85301b1334da7202fb4937608d419a1138
Ayimany <Ayimany@proton.me>
2023-07-25 00:27:33 -0600

    Create bot base

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,10 @@ import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 import registry.CommandRegistry
 
-const val TOKEN: String = "MTEzMzE2MzMyODM1MzYxOTk3OQ.G7rOWd.87GVa0r-gjltMCB9ysaHc-sd2u0eUaYegQyDeQ"
-
 suspend fun main(args: Array<String>) {
     CommandRegistry.registerAll()
 
-    val kord = Kord(TOKEN)
+    val kord = Kord(args[0])
     val botUser = kord.getSelf()
 
     kord.on <MessageCreateEvent> {
